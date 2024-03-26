afb76824aa8a77d66e3b1baf96074718566c784e
Michael Chang <dks50217@gmail.com>
2024-03-27 00:09:48 +0800

    init

---
+++ js/mixin.js
@@ -0,0 +1,25 @@
+﻿const mixin = {
+    data: function () {
+        return {
+            
+        }
+    },
+    methods: {
+        sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        },
+        getJSON(path) {
+            let fullPath = 'json/' + path;
+            const request = axios.get(fullPath)
+                .then(function (response) {
+                    return response.data;
+                }).catch(function (error) {
+                    console.log(error)
+                });
+
+            return {
+                load: request
+            };
+        }
+    }
+}
